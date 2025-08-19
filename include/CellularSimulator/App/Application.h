c0b6cedb74d2558fe92737129814cc1ae1ba8eef
Fyr <denis.filatov200055@gmail.com>
2025-08-19 21:39:24 +0300

    feat: Add basic console runner to test core simulation logic

--- include/CellularSimulator/App/Application.h
+++ include/CellularSimulator/App/Application.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "CellularSimulator/Core/Simulator.h"
+#include <memory>
+
+namespace CellularSimulator
+{
+namespace App
+{
+class Application
+{
+public:
+    Application();
+    ~Application();
+    
+    void Run();
+
+private:
+    std::unique_ptr<Core::Simulator> Sim;
+};
+} // namespace App
+} // namespace CellularSimulator
