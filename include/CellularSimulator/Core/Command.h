75257bdb7af433c5a415aed97709b5f0303fd77d
Fyr <denis.filatov200055@gmail.com>
2025-08-20 23:37:42 +0300

    feat: Define contracts for cell behavior and command pattern

---
+++ include/CellularSimulator/Core/Command.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+// Forward declarations
+namespace CellularSimulator
+{
+namespace Core
+{
+class Simulator;
+class Cell;
+}
+}
+
+namespace CellularSimulator
+{
+namespace Core
+{
+
+/**
+ * @class Command
+ * @brief Base class for commands that can be executed on the simulator for the cell.
+ */
+class Command
+{
+public:
+    virtual ~Command() = default;
+
+    virtual void Execute(Simulator& Sim, Cell& Agent) = 0;
+};
+} // namespace Core
+} // namespace CellularSimulator
