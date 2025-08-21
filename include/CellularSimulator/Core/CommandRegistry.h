09c757155e9920eff81c19374f126797887c667a
Fyr <denis.filatov200055@gmail.com>
2025-08-21 14:21:01 +0300

    feat: Implement MoveForward command

--- include/CellularSimulator/Core/CommandRegistry.h
+++ include/CellularSimulator/Core/CommandRegistry.h
@@ -9,6 +9,11 @@ namespace CellularSimulator
 namespace Core
 {
 
+/**
+ * @brief Creates a command of type TCommand
+ * @tparam TCommand Command type to create that must derive from CellularSimulator::Core::Command class
+ * @return Creation function
+ */
 template <typename TCommand>
 std::function<std::unique_ptr<Command>()> MakeCommandCreator()
 {
@@ -19,9 +24,18 @@ std::function<std::unique_ptr<Command>()> MakeCommandCreator()
     };
 }
 
+/**
+ * @class CommandRegistrar
+ * @brief Registers a command with the command factory
+ */
 class CommandRegistrar
 {
 public:
+    /**
+     * @brief Constructor for registering a command
+     * @param CommandName Name of the command to register
+     * @param CreateFn Creation function for the command. Can be created with MakeCommandCreator() template for any command
+     */
     CommandRegistrar(std::string_view CommandName, std::function<std::unique_ptr<Command>()> CreateFn)
     {
         CommandFactory::RegisterCommand(CommandName, std::move(CreateFn));
---
+++ src/Core/Commands/MoveForwardCommand.cpp
@@ -0,0 +1,40 @@
+﻿#include "CellularSimulator/Core/Commands/MoveForwardCommand.h"
+#include <cstdint>
+#include "CellularSimulator/Core/Cell.h"
+#include "CellularSimulator/Core/CellSimulatorTypes.h"
+#include "CellularSimulator/Core/CommandRegistry.h"
+#include "CellularSimulator/Core/Simulator.h"
+
+using namespace CellularSimulator::Core;
+
+void MoveForwardCommand::Execute(Simulator& Sim, Cell& Agent)
+{
+    const int32_t CurrentX = Agent.GetX();
+    const int32_t CurrentY = Agent.GetY();
+    const EDirection Direction = Agent.GetDirection();
+
+    int32_t NextX = CurrentX;
+    int32_t NextY = CurrentY;
+
+    switch (Direction)
+    {
+        case EDirection::North: NextY--;
+            break;
+        case EDirection::East: NextX++;
+            break;
+        case EDirection::South: NextY++;
+            break;
+        case EDirection::West: NextX--;
+            break;
+        case EDirection::None: break;
+    }
+    if (Sim.IsTileValidAndEmpty(NextX, NextY))
+    {
+        Sim.MoveCell(&Agent, NextX, NextY);
+    }
+}
+
+namespace
+{
+const CommandRegistrar Registrar("MoveForward", MakeCommandCreator<MoveForwardCommand>());
+}
